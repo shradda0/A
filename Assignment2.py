4157386321e5d80c01fef3d32aad19e2d37ede63
shradda0 <89070555+shradda0@users.noreply.github.com>
2021-08-24 14:39:17 +0530

    Assignment2

---
+++ Assignment2.py
@@ -0,0 +1,9 @@
+string=input("Enter any string :")
+a=input("Enter character to check frequency :")
+count=0
+for i in string :
+    if i==a :
+        count+=1
+print(a,"occurs",count,"time(s)")
+
+
